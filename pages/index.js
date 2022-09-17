d30400fc8e9127a51003a5b7078cebb8e0226688
Thales Brederodes <thalesbmc@gmail.com>
2022-09-17 18:10:05 -0300

    feat: adds register pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 import { NextSeo } from "next-seo";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  AvatarBadge,
+  AvatarGroup,
+  Wrap,
+  WrapItem,
+  Avatar,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -7,7 +15,6 @@ export default function Home() {
         title="Projetão 2022.1 SI-UFPE"
         description="Página do projetão 2022.1 SI-UFPE"
       />
-      <div>Salve</div>
     </div>
   );
 }
