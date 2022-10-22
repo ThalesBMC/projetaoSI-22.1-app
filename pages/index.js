f3a64cae3b79af83d3574f82009791e90dbe3be5
Thales Brederodes <thalesbmc@gmail.com>
2022-10-22 16:08:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
 import { NextSeo } from "next-seo";
-import { ChakraProvider } from "@chakra-ui/react";
-import {
-  AvatarBadge,
-  AvatarGroup,
-  Wrap,
-  WrapItem,
-  Avatar,
-} from "@chakra-ui/react";
+
 import { HomePage } from "./home";
 
 export default function Home() {
   return (
     <div>
-      <NextSeo
-        title="Projetão 2022.1 SI-UFPE"
-        description="Página do projetão 2022.1 SI-UFPE"
-      />
       <HomePage />
     </div>
   );
