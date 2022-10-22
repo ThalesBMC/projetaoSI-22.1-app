e10c937a2a6eceb48840b75bfdfc40291f520176
Thales Brederodes <thalesbmc@gmail.com>
2022-10-22 16:26:47 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { LoginContextProvider } from "../contexts/LoginContext";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "",
+        color: "",
+      },
+    }),
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <LoginContextProvider>
         <Component {...pageProps} />
       </LoginContextProvider>
